a2915f09bfb45a59d29b70a6ba04b318f3148d0b
Frithjof Gressmann <frithjof@noc.io>
2024-12-05 15:45:30 -0600

    General update

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,37 +1,57 @@
+import { viteBundler } from '@vuepress/bundler-vite'
+import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
-import type { DefaultThemeOptions } from 'vuepress'
-const { path } = require('@vuepress/utils')
 
-export default defineUserConfig<DefaultThemeOptions>({
-    // site config
+const pages = [
+    {
+        'text': 'Research',
+        'link': 'research.md'
+    },
+    {
+        'text': 'Open Source',
+        'link': 'open-source.md'
+    },
+    {
+        'text': 'Contact',
+        'link': 'contact.md'
+    }
+]
+
+const links = [
+    {
+        'text': 'CV',
+        'link': 'https://drive.google.com/file/d/1NAvtlORsC4J1961VoOO6FLPN2V1FHUZk/view?usp=sharing'
+    },
+    {
+        'text': 'Google Scholar',
+        'link': 'https://scholar.google.com/citations?user=n8fv7j4AAAAJ'
+    },
+    {
+        'text': 'GitHub',
+        'link': 'https://github.com/frthjf'
+    },
+    {
+        'text': 'LinkedIn',
+        'link': 'https://www.linkedin.com/in/frithjof-gressmann-6a1606229/'
+    },
+]
+
+export default defineUserConfig({
+    bundler: viteBundler(),
+    theme: defaultTheme({
+        navbar: links,
+        sidebar: false,
+        // sidebar: [{
+        //     'text': '',
+        //     'children': links,
+        //     'link': 'README.md',
+        // }],
+        themePlugins: {
+            git: false
+        }
+    }),
+
     lang: 'en-US',
     title: 'Frithjof Gressmann',
     description: 'Frithjof Gressmann\'s website',
-
-    // theme and its config
-    theme: path.resolve(__dirname, './theme/index.ts'),
-    themeConfig: {
-        navbar: [
-            {
-                'text': 'Research',
-                'link': 'research.md'
-            },
-            {
-                'text': 'Open Source',
-                'link': 'open-source.md'
-            },
-            {
-                'text': 'Contact',
-                'link': 'contact.md'
-            }
-        ],
-        sidebar: [{
-            'text': '',
-            'link': 'README.md',
-        }],
-        themePlugins: {
-            externalLinkIcon: false,
-            git: false
-        }
-    },
 })
--- src/README.md
+++ src/README.md
@@ -6,8 +6,12 @@ description: Frithjof Gressmann's website
 <img src="./profile.jpg" alt="Frithjof Gressmann" height="200px" style="margin: 5% 0;">
 
 
-Hi, I am **Frithjof Gressmann**. I am a Ph.D. student in the department of Computer Science at the University of Illinois at Urbana-Champaign (UIUC). 
+Hi, I am **Frithjof Gressmann**. I am a Ph.D. student in the department of Computer Science at the University of Illinois at Urbana-Champaign (UIUC).
+
+I work in the [ParasolLab](https://parasollab.web.illinois.edu/) on the [Mind In Vitro project](https://mindinvitro.illinois.edu), a NSF Expedition in Computing program developing the science and technology to fabricate, model and program systems based on living neurons.
+
+My research centers on machine learning and neural simulation techniques to learn to interact with in vitro neuron cultures ([Google Scholar](https://scholar.google.com/citations?user=n8fv7j4AAAAJ), [GitHub](https://github.com/frthjf)).
 
 Before joining Illinois, I studied Physics in [Göttingen](https://www.uni-goettingen.de) with a thesis in the [Computational Neuroscience Group](https://alexandria.physik3.uni-goettingen.de/cns-group/) and obtained a master's degree in Cognitive Science from the [University College London](http://ucl.ac.uk/), where I was advised by [Franz Király](https://www.ucl.ac.uk/statistics/people/franz-kiraly). I also spent time in industry at [Graphcore](https://www.graphcore.ai/).
 
-Please feel free to [reach out](contact.md) to me with any questions, comments, ideas etc.
+Feel free to contact me via [email](mailto:mail@frthjf.com). 
